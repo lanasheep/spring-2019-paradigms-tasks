add8ee57d4fc6d65688eb96c8b9e21826a388e8a
lanasheep <41874174+lanasheep@users.noreply.github.com>
2019-05-04 21:45:05 +0300

    add tests to TestRobots.hs

--- task10/test/TestRobots.hs
+++ task10/test/TestRobots.hs
@@ -8,10 +8,49 @@ main = defaultMain testsRobots
 
 testsRobots :: TestTree
 testsRobots = let
-        walter = robot "Walter" 50 50
+        walter = robot "Walter" 50 70
+        dead_robot = robot "_" 1 0
+        opponent = robot "Opponent" 20 80
     in testGroup "Unit tests for Robots task"
-        [ testCase "Test for getName" $
+        [ testCase "Test for robot constructor" $
+            walter @?= ("Walter", 50, 70)
+
+        , testCase "Test for getName" $
             getName walter @?= "Walter"
+        , testCase "Test for getAttack" $
+            getAttack walter @?= 50
+        , testCase "Test for getHealth" $
+            getHealth walter @?= 70
+
+        , testCase "Test for setName" $
+            setName "Walter1" walter @?= ("Walter1", 50, 70)
+        , testCase "Test for setAttack" $
+            setAttack 30 walter @?= ("Walter", 30, 70)
+        , testCase "Test for setHealth" $
+            setHealth 90 walter @?= ("Walter", 50, 90)
+
         , testCase "Test for printRobot" $
-            printRobot walter @?= "Walter, attack: 50, health: 50"
+            printRobot walter @?= "Walter, attack: 50, health: 70"
+
+        , testCase "Test for damage" $
+            damage walter 30 @?= ("Walter", 50, 40)
+
+        , testCase "Test for isAlive for dead robot" $
+            isAlive dead_robot @?= False
+        , testCase "Test for isAlive for alive robot" $
+            isAlive walter @?= True
+
+        , testCase "Test for fight with dead robot" $
+            fight dead_robot walter @?= walter
+        , testCase "Test for fight with alive robot" $
+            fight opponent walter @?= ("Walter", 50, 50)
+
+        , testCase "Test for threeRoundFight" $
+            threeRoundFight opponent walter @?= ("Opponent", 20, 30)
+
+        , testCase "Test for neueRobotAttak" $
+            neueRobotAttak walter @?= ("Walter", 50, -430)
+
+        , testCase "Test for survivors" $
+            survivors @?= [("Basya", 424, 666)]
         ]
