d827625d6685de7d05d90253b5b25053b551d59b
lanasheep <41874174+lanasheep@users.noreply.github.com>
2019-05-11 04:55:00 +0300

    TestBasics.hs fixed

--- task10/test/TestBasics.hs
+++ task10/test/TestBasics.hs
@@ -21,6 +21,9 @@ testsBasics = testGroup "Unit tests for Basics tasks"
     , testCase "tail' works on non-empty list too" $
         tail' [1,2,3] @?= [2,3]
 
+    , testCase "tail' works on infinite list too" $
+        take 30 (tail' [1..]) @?= take 30 [2..]
+
 
     , testCase "take' takes 1 element from 3-element list" $
         take' 1 [1,2,3] @?= [1]
@@ -38,12 +41,18 @@ testsBasics = testGroup "Unit tests for Basics tasks"
     , testCase "drop' drops 5 element from 3-element list" $
         drop' 5 [1,2,3] @?= []
 
+    , testCase "drop' drops 5 element from infinite list" $
+        take 30 (drop' 5 [1..]) @?= take 30 [6..]
+
 
     , testCase "filter' selects only even numbers from 0 to 10" $
         filter' even [0..10] @?= [0,2..10]
 
     , testCase "filter' selects only odd numbers from 0 to 10" $
         filter' odd [0..10] @?= [1,3..9]
+
+    , testCase "filter' selects only odd numbers from 0 to infinity" $
+        take 30 (filter' odd [0..]) @?= take 30 [1,3..]
 
 
     , testCase "foldl'' can be used for finding sum of elements" $
@@ -52,20 +61,26 @@ testsBasics = testGroup "Unit tests for Basics tasks"
     , testCase "foldl'' can be used for finding multiplication of elements" $
         foldl'' (*) 1 [1,2,3] @?= 6
 
+    , testCase "foldl'' can be used for finding exponentiation of element" $
+        foldl'' (**) 2 [3,2,1] @?= 64
+
 
     , testCase "concat' works on finite lists as expected" $
         concat' [1,2,3] [4,5,6] @?= [1..6]
 
     , testCase "concat' works on empty and non-empty finite lists" $
         concat' [] [2,2,2] @?= [2,2,2]
-        
 
-    , testCase "quickSort actualy sorts the list" $
+    , testCase "concat' works on finite and infinite lists" $
+        take 30 (concat' [1,2,3] [4..]) @?= take 30 [1..]
+
+
+    , testCase "quickSort actualy sorts the sorted descending list" $
         quickSort' [5,2,3,4,1] @?= [1..5]
 
-    , testCase "quickSort actualy sorts the list" $
+    , testCase "quickSort actualy sorts the list with duplicates" $
         quickSort' [2,7,1,9,4,2,7] @?= [1,2,2,4,7,7,9]
 
-    , testCase "quickSort actualy sorts the list" $
+    , testCase "quickSort actualy sorts the sorted descending list with duplicates" $
         quickSort' [3,3,2,2,1] @?= [1,2,2,3,3]
     ]
