6506d00b9c32c1506f462df34252d2dfb6bbc1d3
lanasheep <41874174+lanasheep@users.noreply.github.com>
2019-05-11 04:53:02 +0300

    Basics.hs fixed

--- task10/src/Basics.hs
+++ task10/src/Basics.hs
@@ -13,26 +13,25 @@ head' (x:_) = x
 
 -- 2. tail' возвращает список без первого элемента, для пустого - пустой
 tail' :: [a] -> [a]
-tail' [] = []
+tail' []     = []
 tail' (_:xs) = xs
 
 -- 3. take' возвращает первые n >= 0 элементов исходного списка
 take' :: Int -> [a] -> [a]
-take' 0 _ = []
-take' _ [] = []
-take' n (x:xs) = x:(take' (n - 1) xs)
+take' 0 _      = []
+take' n (x:xs) = x : take' (n - 1) xs
 
 -- 4. drop' возвращает список без первых n >= 0 элементов; если n больше длины
 -- списка, то пустой список.
 drop' :: Int -> [a] -> [a]
-drop' _ [] = []
-drop' 0 l = l
+drop' _ []     = []
+drop' 0 xs     = xs
 drop' n (_:xs) = drop' (n - 1) xs
 
 -- 5. filter' возвращает список из элементов, для которых f возвращает True
 filter' :: (a -> Bool) -> [a] -> [a]
-filter' _ [] = []
-filter' f (x:xs) | f x == True = (x:(filter' f xs))
+filter' _ []     = []
+filter' f (x:xs) | f x       = x : filter' f xs
                  | otherwise = filter' f xs
 
 -- 6. foldl'' последовательно применяет функцию f к элементу списка l и значению,
@@ -40,18 +39,20 @@ filter' f (x:xs) | f x == True = (x:(filter' f xs))
 -- foldl'' (+) 0 [1, 2, 3] == (((0 + 1) + 2) + 3)
 -- foldl'' (*) 4 [] == 4
 foldl'' :: (a -> b -> a) -> a -> [b] -> a
-foldl'' _ z [] = z
+foldl'' _ z []     = z
 foldl'' f z (x:xs) = foldl'' f (f z x) xs
 
 -- 7. concat' принимает на вход два списка и возвращает их конкатенацию
 -- concat' [1,2] [3] == [1,2,3]
 concat' :: [a] -> [a] -> [a]
-concat' [] l = l
-concat' (x:xs) l = (x:(concat' xs l))
+concat' []     xs = xs
+concat' (x:xs) ys = x : concat' xs ys
 
 -- 8. quickSort' возвращает его отсортированный список
 -- quickSort' должен быть реализован через алгоритм QuickSort
 -- (выбор pivot может быть любым)
 quickSort' :: Ord a => [a] -> [a]
-quickSort' [] = []
-quickSort' (x:xs) = concat' (quickSort'(filter' (<x) xs)) (x:(quickSort'(filter' (>=x) xs)))
+quickSort' []     = []
+quickSort' (x:xs) = concat' left right where
+    left  = quickSort' (filter' (<x) xs)
+    right = x : quickSort' (filter' (>=x) xs)
